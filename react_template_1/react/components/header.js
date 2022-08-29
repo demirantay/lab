48b3107c67c0f6a1e4ca2737f97810256943e66f
Demir Antay <demir99antay@gmail.com>
2022-08-29 15:01:11 +0300

    Add files via upload

---
+++ react_template_1/react/components/header.js
@@ -0,0 +1,12 @@
+// css imports
+import './style.css';
+
+// Header Navigation
+function HeadNavigation() {
+  return
+  <div class="navbar">
+    <a href="#">Link</a>
+    <a href="#">Link</a>
+    <a href="#">Link</a>
+  </div>;
+}
